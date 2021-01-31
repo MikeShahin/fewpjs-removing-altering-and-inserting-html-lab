1d971307000fedb0e19ed7fb2466243e02f3e740
MikeShahin <gobias101@gmail.com>
2021-01-31 14:29:29 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+let main = document.getElementById("main");
+
+main.remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+document.body.append(newHeader)
